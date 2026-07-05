010a809bb582b5c37ecfb7c46563affa765f401e
yihshan <160333406+yihshan@users.noreply.github.com>
2026-07-05 20:29:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ try:
     EMAIL_PASSWORD = st.secrets.get("EMAIL_PASSWORD", os.getenv("EMAIL_PASSWORD", ""))
     GEMINI_API_KEY = st.secrets.get("GEMINI_API_KEY", os.getenv("GEMINI_API_KEY", ""))
 
-    #LINE_CHANNEL_ACCESS_TOKEN = st.secrets.get("LINE_CHANNEL_ACCESS_TOKEN", os.getenv("LINE_CHANNEL_ACCESS_TOKEN", ""))
-    #LINE_USER_ID = st.secrets.get("LINE_USER_ID", os.getenv("LINE_USER_ID", ""))
+    LINE_CHANNEL_ACCESS_TOKEN = st.secrets.get("LINE_CHANNEL_ACCESS_TOKEN", os.getenv("LINE_CHANNEL_ACCESS_TOKEN", ""))
+    LINE_USER_ID = st.secrets.get("LINE_USER_ID", os.getenv("LINE_USER_ID", ""))
     
 except Exception:
     # 👑 2. 降級回歸 GitHub Actions / 本地環境憑證鏈
@@ -29,14 +29,14 @@ except Exception:
     EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD", "")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 
-    #LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", "")
-    #LINE_USER_ID = os.getenv("LINE_USER_ID", "")
+    LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", "")
+    LINE_USER_ID = os.getenv("LINE_USER_ID", "")
 
 # 🛠️ 終極防禦：如果在 Actions 區塊讀取失敗，嘗試直接做全域強抓
-#if not LINE_CHANNEL_ACCESS_TOKEN:
-    #LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN", "")
-#if not LINE_USER_ID:
-    #LINE_USER_ID = os.environ.get("LINE_USER_ID", "")
+if not LINE_CHANNEL_ACCESS_TOKEN:
+    LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN", "")
+if not LINE_USER_ID:
+    LINE_USER_ID = os.environ.get("LINE_USER_ID", "")
     
 # 收件人清單
 RECIPIENTS = [
@@ -45,10 +45,10 @@ RECIPIENTS = [
 
 # 密件副本收件人清單
 BCC_RECIPIENTS = [
-    #"pp4740qq@gmail.com",
-    #"a0939390292@gmail.com",
-    #"sk.hsieh@yahoo.com.tw",
-    #"ywho6907@gmail.com"
+    "pp4740qq@gmail.com",
+    "a0939390292@gmail.com",
+    "sk.hsieh@yahoo.com.tw",
+    "ywho6907@gmail.com"
 ]
 
 # 技術分析參數
